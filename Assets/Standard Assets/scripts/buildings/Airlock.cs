ac20a3e5541b72f8edcc123ab02921d5a7370f66
eazama <eazama001@gmail.com>
2014-05-05 12:37:10 -0700

    Added Airlock Building
    framework for oxygen diffusing through the colony from life supports

---
+++ Assets/Standard Assets/scripts/buildings/Airlock.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Airlock : Corridor {
+
+	private bool isopen = false;
+
+	public bool isOpen { get { return isopen; } }
+
+	public void setOpen(bool open) {
+		isopen = open;
+	}
+
+}
