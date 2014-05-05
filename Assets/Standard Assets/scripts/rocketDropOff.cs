3e065641e966148943d5ab00204e0396e4092902
eazama <eazama001@gmail.com>
2014-05-05 14:48:15 -0700

    Condensed resources into Dictionary

--- Assets/Standard Assets/scripts/rocketDropOff.cs
+++ Assets/Standard Assets/scripts/rocketDropOff.cs
@@ -1,41 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class rocketDropOff : MonoBehaviour {
-	public float power;
-	public float water;
-	public float oxygen;
-	public float food;
-	public float population;
-	public float materials;
-	public float delay; 
-	public float dropRate; //how often drops happen in seconds
+
+	Dictionary<ResourceType, float> ResourcesPerDrop = new Dictionary<ResourceType, float>();
+
+	public float delay = 10; 
+	public float dropRate = 30; //how often drops happen in seconds
 	
 	public GameObject building;
 	private gameManager gameController;
 	
 	// Use this for initialization
 	void Start () {
+		ResourcesPerDrop[ResourceType.Power] = 100;
+		ResourcesPerDrop[ResourceType.Water] = 100;
+		ResourcesPerDrop[ResourceType.Oxygen] = 100;
+		ResourcesPerDrop[ResourceType.Food] = 100;
+		ResourcesPerDrop[ResourceType.Population] = 1;
+		ResourcesPerDrop[ResourceType.Materials] = 100;
+
 		GameObject gameControllerObject = GameObject.Find ("gameManager");
 		gameController = gameControllerObject.GetComponent <gameManager>();
 		InvokeRepeating ("dropOff", delay, dropRate);
 
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	void dropOff(){
-		/*
-		gameController.addPower(power);
-		gameController.addWater(water);
-		gameController.addOxygen(oxygen);
-		gameController.addFood(food);
-		gameController.addPopulation(population);
-		gameController.addMaterials(materials);
-		*/
+		foreach(KeyValuePair<ResourceType, float> ent in ResourcesPerDrop){
+			gameController.addResource(ent.Key, ent.Value);
+		}
 	}
 
 }
