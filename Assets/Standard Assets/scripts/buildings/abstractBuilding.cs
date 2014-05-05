537d0ae4c0b03fffd30060c9b6f5defd6ea940c2
eazama <eazama001@gmail.com>
2014-05-05 12:48:29 -0700

    added isActive property
    Allows buildings to be turned on or off

--- Assets/Standard Assets/scripts/buildings/abstractBuilding.cs
+++ Assets/Standard Assets/scripts/buildings/abstractBuilding.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+ * Base Building to inherit from. Adds no resources if somehow instantiated.
+ */
 public class abstractBuilding : MonoBehaviour {
 
 	public float power = 0; //Units of Power produced per update
@@ -15,20 +18,32 @@ public class abstractBuilding : MonoBehaviour {
 
 	private gameManager gameController;
 
+	//Determines if building is actively producing resources
+	public bool isActive{ get { return isactive; } }
+
+	private bool isactive = true;
+
+	//Sets if building is actively producing resources
+	public void setActive(bool active) {
+		isactive = active;
+	}
+
 	// Use this for initialization
-	protected virtual void Start () {
-		GameObject gameControllerObject = GameObject.Find ("gameManager");
+	protected virtual void Start() {
+		GameObject gameControllerObject = GameObject.Find("gameManager");
 		gameController = gameControllerObject.GetComponent <gameManager>();
 	}
 	
 	// Update is called once per frame
-	protected virtual void Update () {
-		//BEGIN add resources to pool
-		gameController.addPower(power);
-		gameController.addWater(water);
-		gameController.addOxygen(oxygen);
-		gameController.addFood(food);
-		gameController.addMaterials(materials);
-		//END add resources to pool
+	protected virtual void Update() {
+		//BEGIN add resources to pool if active
+		if (isActive) {
+			gameController.addPower(power);
+			gameController.addWater(water);
+			gameController.addOxygen(oxygen);
+			gameController.addFood(food);
+			gameController.addMaterials(materials);
+		}
+		//END add resources to pool if active
 	}
 }
