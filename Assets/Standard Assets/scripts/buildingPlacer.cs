368c66551d80ba60cc15cfa0863c58345d36bb84
Milan Gurung <mgurung@ucsc.edu>
2014-05-06 19:24:18 -0700

    Added grid stuff

--- Assets/Standard Assets/scripts/buildingPlacer.cs
+++ Assets/Standard Assets/scripts/buildingPlacer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class buildingPlacer : MonoBehaviour {
 	//public GameObject building;
+	public int gridSize = 20;
 	
 	// Use this for initialization
 	void Start () {
@@ -13,12 +14,15 @@ public class buildingPlacer : MonoBehaviour {
 	void Update () {
 		float distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
 		Vector3 pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
-		transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
+		transform.position = new Vector3(Mathf.Round(pos_move.x/gridSize)*gridSize,
+		                                 Mathf.Round(pos_move.y/gridSize)*gridSize,
+		                                 Mathf.Round(pos_move.z/gridSize)*gridSize);
 		
 	}
 	
 	void OnMouseUp(){
 		//Instantiate (this, this.transform.position, new Quaternion (0, 0, 0, 0));
+
 		Destroy (this);
 	}
 }
