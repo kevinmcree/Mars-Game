336fc7a7797056b14c8edc474b9140e97bdb8aa0
Kevin Mcree <kevinmcree@gmail.com>
2014-05-07 19:32:19 -0700

    Added water mines and a sprite for it

--- Assets/Standard Assets/scripts/buildings/greenHouse.cs
+++ Assets/Standard Assets/scripts/buildings/greenHouse.cs
@@ -6,8 +6,8 @@ public class greenHouse : abstractBuilding {
 	public float manpower = 0;
 
 	public greenHouse(){
-		ResourcePerUpdate[ResourceType.Power] = .005f;
-		ResourcePerUpdate[ResourceType.Water] = .005f;
+		ResourcePerUpdate[ResourceType.Power] = -.005f;
+		ResourcePerUpdate[ResourceType.Water] = -.005f;
 		ResourcePerUpdate[ResourceType.Oxygen] = .005f;
 		ResourcePerUpdate[ResourceType.Food] = .01f;
 
---
+++ Assets/Standard Assets/scripts/buildings/waterMine.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterMine : abstractBuilding {	
+	public waterMine(){
+		ResourcePerUpdate[ResourceType.Water] = .01f;
+		ResourcePerUpdate[ResourceType.Power] = -.01f;
+
+		ResourceToBuild [ResourceType.Materials] = 50;
+	}
+}
