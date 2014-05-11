73bfdd4beca831b9042c64f70974ceb84ed25c93
eazama <eazama001@gmail.com>
2014-05-11 11:58:07 -0700

    Added Camera Controls
    Right Click to move camera around, scroll wheel to zoom

---
+++ Assets/Standard Assets/scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	Vector3 old_pos = Vector3.zero;
+
+	// Use this for initialization
+	void Start() {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		float cameraSize = this.GetComponent<Camera>().orthographicSize;
+		if (Input.GetMouseButton(1)) {
+			Vector3 new_pos = Input.mousePosition;
+			float ratio = Screen.height/(cameraSize*2);
+			new_pos /= ratio;
+			if(old_pos == Vector3.zero){old_pos = new_pos;}
+			Vector3 delta = new_pos - old_pos;
+			print(Screen.height + " " + Screen.width);
+
+			transform.position = transform.position-delta;
+			old_pos = new_pos;
+		} else {
+			old_pos = Vector3.zero;
+		}
+		float newSize = Mathf.Clamp(cameraSize - Input.GetAxis("Mouse ScrollWheel")*60, 100, 400);
+		this.GetComponent<Camera>().orthographicSize = newSize;
+	}
+}
