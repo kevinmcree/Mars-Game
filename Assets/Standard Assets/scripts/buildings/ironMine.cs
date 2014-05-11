f1abcdd9ce98f14f0fb3fa26bd4f6eb93beb4c20
Milan Gurung <mgurung@ucsc.edu>
2014-05-10 18:42:19 -0700

    Added iron mines, not sprite yet

---
+++ Assets/Standard Assets/scripts/buildings/ironMine.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ironMine : abstractBuilding {	
+	public ironMine(){
+		ResourcePerUpdate[ResourceType.Materials] = .01f;
+		ResourcePerUpdate[ResourceType.Power] = -.02f;
+		ResourcePerUpdate[ResourceType.Oxygen] = -.001f;
+		
+		ResourceToBuild [ResourceType.Materials] = 25;
+	}
+}
--- Assets/Standard Assets/scripts/buildings/waterMine.cs
+++ Assets/Standard Assets/scripts/buildings/waterMine.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class waterMine : abstractBuilding {	
 	public waterMine(){
 		ResourcePerUpdate[ResourceType.Water] = .01f;
-		ResourcePerUpdate[ResourceType.Power] = -.01f;
+		ResourcePerUpdate[ResourceType.Power] = -.02f;
+		ResourcePerUpdate[ResourceType.Oxygen] = -.001f;
 
 		ResourceToBuild [ResourceType.Materials] = 50;
 	}
